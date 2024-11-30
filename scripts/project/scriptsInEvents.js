d6d33ee2ba63b6df5afbff3ad93c005af194a4d8
julisunkan <julisunkan@gmail.com>
2024-11-30 17:22:12 +0100

    Added

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async WordGameSheet_Event13_Act4(runtime, localVars)
+	{
+		globalThis.GetAndCheckWord(runtime);
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
